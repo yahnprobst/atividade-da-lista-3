c8d477a7004164607d64c55c6f3816740291a093
nathankammer <nathan_k_silva@estudante.sesisenai.org.br>
2026-05-11 17:05:03 -0300

    Add files via upload

---
+++ atividade-da-lista-3-master/atividade-da-lista-3-master/exercicio-2-lista-3.js
@@ -0,0 +1,12 @@
+// exercicio 2
+
+function calculator (compradas, vendidas) {
+    let a = compradas = compradas * 25;
+    let b = vendidas = vendidas * 40;
+
+    let calculator = b - a;
+
+    return "O lucro é de R$" + calculator;
+
+}
+console.log(calculator(1,2));

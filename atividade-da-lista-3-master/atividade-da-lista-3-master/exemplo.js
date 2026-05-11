9e80cd2b162e56eb2505fb2ca745827e0ddd002c
nathankammer <nathan_k_silva@estudante.sesisenai.org.br>
2026-05-11 17:04:20 -0300

    Add files via upload

---
+++ atividade-da-lista-3-master/atividade-da-lista-3-master/exemplo.js
@@ -0,0 +1,11 @@
+// função soma
+
+function soma (valor1, valor2) {
+let a = valor1;
+let b = valor2;
+let soma = a + b;
+
+return soma;
+}
+
+console.log(soma(3,9999));
---
+++ atividade-da-lista-3-master/atividade-da-lista-3-master/exercicio-1-lista-3.js
@@ -0,0 +1,11 @@
+// exercicio 1
+
+function calculator (caderno, quantidade) {
+    const a = caderno = 12.50;
+    let b = quantidade;
+    let calculator = caderno * quantidade;
+
+    return "R$" + calculator;
+}
+
+console.log(calculator(1,3));
